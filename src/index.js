67b8fd40157cb33561a85e00fc1c58c5127c0599
Anton Stepurko <stepurko.anton@gmail.com>
2021-03-07 13:35:14 +0300

    feat: implemented function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let bracket of str) {
+        for (let arr_bracket of bracketsConfig) {
+            
+            if (!arr_bracket.includes(bracket)) continue;
+
+            switch (bracket) {
+                case arr_bracket[0]:
+                    if (stack[stack.length - 1] == bracket && arr_bracket[0] == arr_bracket[1]) stack.pop();
+                    else stack.push(bracket);
+                    break;
+
+                case arr_bracket[1]:
+                    if (stack.length == 0) return false;
+                    if (arr_bracket[0] != stack.pop()) return false;
+            
+                default:
+                    break;
+            }
+        }
+        // console.log(stack);
+    }
+
+    return stack.length == 0 ? true : false;
 }
